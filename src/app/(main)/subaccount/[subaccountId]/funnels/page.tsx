3c0d07f91b2132d91d3916a1a6ca947a20d64290
Mohmmed Aariz <mohmmed.ariz786@gmail.com>
2024-11-10 16:30:44 +0530

    added funnel page

--- src/app/(main)/subaccount/[subaccountId]/funnels/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/page.tsx
@@ -1,18 +1,18 @@
-import { getFunnels } from '@/lib/queries'
+// import { getFunnels } from '@/lib/queries'
 import React from 'react'
-import FunnelsDataTable from './data-table'
-import { Plus } from 'lucide-react'
-import { columns } from './columns'
-import FunnelForm from '@/components/forms/funnel-form'
+// import FunnelsDataTable from './data-table'
+// import { Plus } from 'lucide-react'
+// import { columns } from './columns'
+// import FunnelForm from '@/components/forms/funnel-form'
 import BlurPage from '@/components/global/blur-page'
 
 const Funnels = async ({ params }: { params: { subaccountId: string } }) => {
-  const funnels = await getFunnels(params.subaccountId)
-  if (!funnels) return null
+  // const funnels = await getFunnels(params.subaccountId)
+  // if (!funnels) return null
 
   return (
     <BlurPage>
-      <FunnelsDataTable
+      {/* <FunnelsDataTable
         actionButtonText={
           <>
             <Plus size={15} />
@@ -25,7 +25,8 @@ const Funnels = async ({ params }: { params: { subaccountId: string } }) => {
         filterValue="name"
         columns={columns}
         data={funnels}
-      />
+      /> */}
+      Funnel Page
     </BlurPage>
   )
 }
